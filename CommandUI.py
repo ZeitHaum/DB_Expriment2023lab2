b9317024a4f9861a244c63b1f11fe80da5bbaf50
zeithaum@qq.com <zeithaum@qq.com>
2023-05-24 11:54:35 +0800

    完成实验开发,实现数据库的增删查改

--- CommandUI.py
+++ CommandUI.py
@@ -20,14 +20,16 @@ def isnum(string):
 def error(bl:bool,error_msg:str)->None:
     if(not bl):
         print("Error! "+ error_msg)
-    assert(bl)
+        print("输入任意内容回到开始菜单")
+        input()
+        begin_service()
     
 def todo():
     error(False,"该部分未完成!")
     
 def deal_service(opt:int,db_name_id:int)->None:
-    now_db = db_lis[db_name_id]
     if(opt==1):
+        now_db = db_lis[db_name_id]
         #增添
         print("请输入{0}属性值(注意外键应确保存在,否则请输出null)".format(db_names[db_name_id]))
         val_lis = []
@@ -47,16 +49,154 @@ def deal_service(opt:int,db_name_id:int)->None:
             
     elif(opt==2):
         #删除
-        todo()
+        print("""
+菜单(请选择删除的方式)
+1.按主键删除
+2.按属性值删除(删除所有属性值为指定值的数据)
+3.按属性范围删除(删除所有属性值为指定范围的数据)
+4.退出
+              """)
+        del_opt = int(input())
+        error(del_opt>=1 and del_opt<=4,"错误的输入")
+        attr = str()
+        lval = str()
+        rval = str()
+        if(del_opt==1):
+            print("请输入主键值(名称:{0},类型{1}):".format(db_lis[db_name_id].attribute_list[0],str(db_lis[db_name_id].attribute_list_type[0])),end = "")
+            attr = db_lis[db_name_id].attribute_list[0]
+            lval = db_lis[db_name_id].attribute_list_type[0](input())
+            rval = lval
+        elif(del_opt==2):
+            available_str = Operations.list_to_tuplestr_withoutSymbol(db_lis[db_name_id].attribute_list)
+            print("请输入属性名,可选的属性名有{0}:".format(available_str),end="")
+            attr = str(input())
+            at_id = db_lis[db_name_id].attribute_list.index(attr)
+            print("请输入属性值,属性类型应为{0}:".format(str(db_lis[db_name_id].attribute_list_type[at_id])),end="")
+            lval = db_lis[db_name_id].attribute_list_type[at_id](input())
+            rval = lval
+            
+        elif(del_opt==3):
+            available_str = Operations.list_to_tuplestr_withoutSymbol(db_lis[db_name_id].attribute_list) 
+            print("请输入属性名,可选的属性名有{0}:".format(available_str),end="")
+            attr = str(input())
+            at_id = db_lis[db_name_id].attribute_list.index(attr)          
+            print("请输入属性下限(包含),属性类型应为{0}:".format(str(db_lis[db_name_id].attribute_list_type[at_id])),end="")
+            lval = db_lis[db_name_id].attribute_list_type[at_id](input())
+            print("请输入属性上限(包含),属性类型应为{0}:".format(str(db_lis[db_name_id].attribute_list_type[at_id])),end="")
+            rval = db_lis[db_name_id].attribute_list_type[at_id](input())
+        else:
+            myexit()
+        Operations.delete(db_connection,db_names[db_name_id],attr,lval,rval)
     elif(opt==3):
         #查询
-        todo()
+        print("""
+菜单(请选择查询的方式)
+1.显示该表所有数据
+2.按主键查询
+3.按属性值查询(查询所有属性值为指定值的数据)
+4.按属性范围查询(查询所有属性值为指定范围的数据)
+5.退出
+              """)
+        query_opt = int(input())
+        error(query_opt>=1 and query_opt<=4,"错误的输入")
+        attr = str()
+        lval = str()
+        rval = str()
+        if(query_opt==1):
+            attr = None
+        elif(query_opt==2):
+            print("请输入主键值(名称:{0},类型{1}):".format(db_lis[db_name_id].attribute_list[0],str(db_lis[db_name_id].attribute_list_type[0])),end = "")
+            attr = db_lis[db_name_id].attribute_list[0]
+            lval = db_lis[db_name_id].attribute_list_type[0](input())
+            rval = lval
+        elif(query_opt==3):
+            available_str = Operations.list_to_tuplestr_withoutSymbol(db_lis[db_name_id].attribute_list)
+            print("请输入属性名,可选的属性名有{0}:".format(available_str),end="")
+            attr = str(input())
+            at_id = db_lis[db_name_id].attribute_list.index(attr)
+            print("请输入属性值,属性类型应为{0}:".format(str(db_lis[db_name_id].attribute_list_type[at_id])),end="")
+            lval = db_lis[db_name_id].attribute_list_type[at_id](input())
+            rval = lval
+            
+        elif(query_opt==4):
+            available_str = Operations.list_to_tuplestr_withoutSymbol(db_lis[db_name_id].attribute_list) 
+            print("请输入属性名,可选的属性名有{0}:".format(available_str),end="")
+            attr = str(input())
+            at_id = db_lis[db_name_id].attribute_list.index(attr)          
+            print("请输入属性下限(包含),属性类型应为{0}:".format(str(db_lis[db_name_id].attribute_list_type[at_id])),end="")
+            lval = db_lis[db_name_id].attribute_list_type[at_id](input())
+            print("请输入属性上限(包含),属性类型应为{0}:".format(str(db_lis[db_name_id].attribute_list_type[at_id])),end="")
+            rval = db_lis[db_name_id].attribute_list_type[at_id](input())
+        else:
+            myexit()
+        Operations.query(db_connection,db_names[db_name_id],attr,lval,rval)
     elif(opt==4):
         #修改
-        todo()
+        print("""
+菜单(请选择修改的方式)
+1.按主键修改
+2.按属性值修改(修改所有属性值为指定值的数据)
+3.按属性范围修改(修改所有属性值为指定范围的数据)
+4.退出
+              """)
+        up_opt = int(input())
+        error(up_opt>=1 and up_opt<=4,"错误的输入")
+        attr = str()
+        lval = str()
+        rval = str()
+        new_attr = str()
+        new_val = str()
+        if(up_opt==1):
+            print("请输入主键值(名称:{0},类型{1}):".format(db_lis[db_name_id].attribute_list[0],str(db_lis[db_name_id].attribute_list_type[0])),end = "")
+            attr = db_lis[db_name_id].attribute_list[0]
+            lval = db_lis[db_name_id].attribute_list_type[0](input())
+            rval = lval
+            print("请输入要修改的键值名,可选的属性名有{0}:".format(available_str),end="")
+            new_attr = str(input())
+            print("请输入修改后的属性值,属性类型应为{0}:".format(str(db_lis[db_name_id].attribute_list_type[at_id])),end="")
+            new_val = db_lis[db_name_id].attribute_list_type[at_id](input())
+        elif(up_opt==2):
+            available_str = Operations.list_to_tuplestr_withoutSymbol(db_lis[db_name_id].attribute_list[1:])
+            print("请输入属性名,可选的属性名有{0}:".format(available_str),end="")
+            attr = str(input())
+            at_id = db_lis[db_name_id].attribute_list.index(attr)
+            print("请输入属性值,属性类型应为{0}:".format(str(db_lis[db_name_id].attribute_list_type[at_id])),end="")
+            lval = db_lis[db_name_id].attribute_list_type[at_id](input())
+            rval = lval
+            print("请输入要修改的键值名,可选的属性名有{0}:".format(available_str),end="")
+            new_attr = str(input())
+            if(new_attr==db_lis[db_name_id].attribute_list[0]):
+                error(0,"不可以修改主键值!")
+            print("请输入修改后的属性值,属性类型应为{0}:".format(str(db_lis[db_name_id].attribute_list_type[at_id])),end="")
+            new_val = db_lis[db_name_id].attribute_list_type[at_id](input())
+        elif(up_opt==3):
+            available_str = Operations.list_to_tuplestr_withoutSymbol(db_lis[db_name_id].attribute_list) 
+            print("请输入属性名,可选的属性名有{0}:".format(available_str),end="")
+            attr = str(input())
+            at_id = db_lis[db_name_id].attribute_list.index(attr)          
+            print("请输入属性下限(包含),属性类型应为{0}:".format(str(db_lis[db_name_id].attribute_list_type[at_id])),end="")
+            lval = db_lis[db_name_id].attribute_list_type[at_id](input())
+            print("请输入属性上限(包含),属性类型应为{0}:".format(str(db_lis[db_name_id].attribute_list_type[at_id])),end="")
+            rval = db_lis[db_name_id].attribute_list_type[at_id](input())
+            print("请输入要修改的键值名,可选的属性名有{0}:".format(available_str),end="")
+            new_attr = str(input())
+            print("请输入修改后的属性值,属性类型应为{0}:".format(str(db_lis[db_name_id].attribute_list_type[at_id])),end="")
+            new_val = db_lis[db_name_id].attribute_list_type[at_id](input())
+        else:
+            myexit()
+        Operations.update(db_connection,db_names[db_name_id],attr,lval,rval,new_attr,new_val)
     elif(opt==5):
         #执行SQL语句
-        todo()
+        print("请输入命令:",end="")
+        command = str(input())
+        db_cursor = db_connection.cursor()
+        db_cursor.execute(command)
+        db_connection.commit()
+        try:
+            print(db_cursor.fetchall())
+        except:
+            pass
+        print("Log(SQL): Execution successfully complete.")
     else:
         error(0)
     print("输入任意内容回到开始菜单")
@@ -79,7 +219,8 @@ def begin_service():
     if(opt==6):
         myexit()
     else:
-        print("""
+        if(opt<=4):
+            print("""
 菜单(请选择数据库表):
 1.Employee
 2.SalaryInfo
@@ -88,13 +229,15 @@ def begin_service():
 5.AttendanceInfo
 6.Pacitipate_project
 7.退出程序    
-              """)
-        db_name_id = int(input())
-        error(opt>=1 and opt<=7,"错误的输入")
-        if(db_name_id==7):
-            myexit()
+                """)
+            db_name_id = int(input())
+            error(opt>=1 and opt<=7,"错误的输入")
+            if(db_name_id==7):
+                myexit()
+            else:
+                deal_service(opt,db_name_id-1)
         else:
-            deal_service(opt,db_name_id-1)
+            deal_service(opt,None)
         
 
 if __name__ == "__main__":
--- Operations.py
+++ Operations.py
@@ -20,4 +20,43 @@ def add(db_connection,db_name:str,values:list,attr_lis)->None:
     db_cursor.execute(command)
     db_connection.commit()
     print("Log(add): add successfully!")
+
+def delete(db_connection,db_name:str,attr:str,lval:str,rval:str)->None:
+    command = ""
+    if(lval==rval and type(lval)!=int):
+        command = "DELETE FROM " + db_name + " WHERE " + attr + " =  \'" + str(lval) + "\'"
+    else:
+        command = "DELETE FROM " + db_name + " WHERE " + attr + " >= " + str(lval) + " AND " + attr + " <= " + str(rval)
+    print("Log(del): delete command:{0}".format(command))
+    db_cursor = db_connection.cursor()
+    db_cursor.execute(command)
+    db_connection.commit()
+    print("Log(del): delete successfully!")
     
+def query(db_connection,db_name:str,attr:str,lval:str,rval:str)->None:
+    command = ""
+    if(attr == None):
+        command = "SELECT * from " + db_name
+    else:
+        if(lval==rval and type(lval)!=int):
+            command = "SELECT * from " + db_name + " WHERE " + attr + "= \'" + str(lval) + "\'"
+        else:
+            command = "SELECT * from " + db_name + " WHERE " + attr + ">=" + str(lval) + " AND " + attr + " <= " + str(rval)
+    print("Log(query): query command:{0}".format(command))
+    db_cursor = db_connection.cursor()
+    db_cursor.execute(command)
+    db_connection.commit()
+    print("查找结果:" + str(db_cursor.fetchall()))
+    print("Log(query): query successfully complete.")
+    
+def update(db_connection,db_name:str,attr:str,lval:str,rval:str,new_attr:str,new_val:str)->None:
+    command = ""
+    if(lval==rval and type(lval)!=int):
+        command = "UPDATE " + db_name + " SET " + str(new_attr) + " = " + str(new_val) + " WHERE " + attr + " = \'" + str(lval) + "\'"
+    else:
+        command = "UPDATE " + db_name + " SET " + str(new_attr) + " = " + str(new_val) + " WHERE " + attr + " >= " + str(lval) + " AND " + attr + " <= " + str(rval)
+    print("Log(update): query command:{0}".format(command))
+    db_cursor = db_connection.cursor()
+    db_cursor.execute(command)
+    db_connection.commit()
+    print("Log(update): query successfully complete.")
